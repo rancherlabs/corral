3ff527d204e288b0609b6faf19cf98b86dd15a74
Jacob Payne <paynejacob21@gmail.com>
2022-04-14 17:18:20 +0000

    fixed bastion address not being used

--- pkg/corral/node.go
+++ pkg/corral/node.go
@@ -1,9 +1,9 @@
 package corral
 
 type Node struct {
-	Name           string `yaml:"name,omitempty"`
-	User           string `yaml:"user,omitempty"`
-	Address        string `yaml:"address,omitempty"`
-	BastionAddress string `yaml:"bastion_address,omitempty"`
-	OverlayRoot    string `yaml:"overlay_root"`
+	Name           string `json:"name,omitempty" yaml:"name,omitempty"`
+	User           string `json:"user,omitempty" yaml:"user,omitempty"`
+	Address        string `json:"address,omitempty" yaml:"address,omitempty"`
+	BastionAddress string `json:"bastion_address,omitempty" yaml:"bastion_address,omitempty"`
+	OverlayRoot    string `json:"overlay_root" yaml:"overlay_root"`
 }
