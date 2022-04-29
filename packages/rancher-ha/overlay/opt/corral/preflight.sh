141902709ee27542300193c80137662cc317d481
Jacob Payne <paynejacob21@gmail.com>
2022-04-29 18:11:34 +0000

    remove packages

--- packages/rancher-ha/overlay/opt/corral/preflight.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "$CORRAL_corral_user_public_key" >> /$(whoami)/.ssh/authorized_keys
-
-apt install -y jq
-
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
