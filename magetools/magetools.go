395a354ba9204265f7a6cb41e95e5cbffe1c2c8d
Jake Hyde <jakefhyde@gmail.com>
2022-09-16 22:13:36 -0400

    Updated magefiles

--- magetools/magetools.go
+++ magetools/magetools.go
@@ -33,9 +33,20 @@ func boolToIntString(b bool) string {
 	return map[bool]string{true: "1", false: "0"}[b]
 }
 
-func (g *Go) Build(target, output string) error {
+func (g *Go) Build() error {
 	envs := map[string]string{"GOOS": g.OS, "GOARCH": g.Arch, "CGO_ENABLED": g.CGoEnabled, "MAGEFILE_VERBOSE": g.Verbose}
-	return sh.RunWithV(envs, "go", "build", "-a", "-o", output, "--ldflags="+g.versionFlag(), target)
+	return sh.RunWithV(envs, "go", "build", "-a", "-o", g.output(), "--ldflags="+g.versionFlag(), "main.go")
+}
+
+func (g *Go) output() string {
+	version := strings.Split(g.Version, "+")[0]
+	prefix := fmt.Sprintf("dist/corral-%s", version)
+	if g.OS == "windows" {
+		return prefix + ".exe"
+	} else if g.OS == "linux" {
+		return prefix + "-" + g.Arch
+	}
+	return prefix + "-" + g.OS + "-" + g.Arch
 }
 
 func (g *Go) Test(coverpkg string, targets ...string) error {
@@ -80,13 +91,21 @@ func (g *Go) Lint(targets ...string) error {
 }
 
 func (g *Go) versionFlag() string {
-	return fmt.Sprintf(`-X 'github.com/rancherlabs/corral/pkg/versionFlag.Version=%s'`, g.Version)
+	return fmt.Sprintf(`-X 'github.com/rancherlabs/corral/pkg/version.Version=%s'`, g.Version)
 }
+
+var ErrDirtyRepo = errors.New("encountered dirty repo")
 
 func GetCommit() (string, error) {
 	result, err := sh.Output("git", "rev-parse", "--short", "HEAD")
 	if err != nil {
 		return "", err
+	}
+	if err = IsGitClean(); err != nil {
+		if !errors.Is(err, ErrDirtyRepo) {
+			return "", err
+		}
+		result += "-dirty"
 	}
 	return strings.TrimSpace(result), nil
 }
@@ -97,7 +116,7 @@ func IsGitClean() error {
 		return err
 	}
 	if result != "" {
-		return errors.New("encountered dirty repo")
+		return ErrDirtyRepo
 	}
 	return nil
 }
